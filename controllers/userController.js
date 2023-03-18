de4f27ae664227f4b4a84cd707a575d587b76df0
Terre8055 <michaelappiah2018@icloud.com>
2023-03-18 14:18:07 +0000

    Respond json with req.user

--- controllers/userController.js
+++ controllers/userController.js
@@ -89,7 +89,7 @@ const loginUser = asyncHandler(async (req,res) => {
 //access private
 
 const currentUser = asyncHandler(async (req,res) => {
-    res.json({message: 'Current info of user'})
+    res.json(req.user)
 }) 
 
 
