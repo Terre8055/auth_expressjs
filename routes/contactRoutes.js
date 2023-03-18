9cbc674ad564c1343c7f38c0613b75a00c183307
Terre8055 <michaelappiah2018@icloud.com>
2023-03-18 14:29:41 +0000

    Import module (validateToken) as middleware

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const router = express.Router()
 const {getContact, createContact, updateContactID, deleteContact, getContactID} = require('../controllers/contactController')
+const validateToken = require('../middlewares/validateTokenHandler')
 
+router.use(validateToken) //middleware to validate token on all routes
 //The router.route() method is used to define a route for the root path (/) of the /api/contacts endpoint
 
 router.route('/')
